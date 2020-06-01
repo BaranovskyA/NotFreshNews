f67fccf4eea0aee266dc34dc0266e3272c159e94
BaranovskyA <1423demon@mail.ru>
2020-06-02 00:06:56 +0600

    Added username authorization

--- app/News.php
+++ app/News.php
@@ -9,17 +9,4 @@ class News extends Model
     protected $fillable = [
         'title', 'content', 'user_id', 'category_list'
     ];
-
-    public function getResult(string $category = '')
-    {
-        $query = [];
-        if ($category) {
-            $query = $query + ['category' => $category];
-        }
-        if (empty($query)) {
-            return self::all();
-        }
-
-        return $this->where($query)->get();
-    }
 }
